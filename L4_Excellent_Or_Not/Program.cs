06bc1b9f5fb759c96ae2e988ef6305b39faed8e8
ivaylovivanov <ivalentinov@gmail.com>
2016-06-02 00:11:25 +0300

    L4

---
+++ L4_Excellent_Or_Not/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L4_Excellent_Or_Not
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var grade = double.Parse(Console.ReadLine());
+            if (grade >= 5.5)
+            {
+                Console.WriteLine("Excellent!");
+            }
+            else
+            {
+                Console.WriteLine("Not excellent.");
+            }
+        }
+    }
+}
