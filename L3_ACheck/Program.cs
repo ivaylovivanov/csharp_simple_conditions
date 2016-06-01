17d3170addb7f3af85032e2d35e826bbee934c2f
ivaylovivanov <ivalentinov@gmail.com>
2016-06-01 23:28:43 +0300

    L3

---
+++ L3_ACheck/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L3_ACheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var evaluation = double.Parse(Console.ReadLine());
+            if (evaluation >= 5.5)
+            {
+                Console.WriteLine("Excellent!");
+            }
+        }
+    }
+}
