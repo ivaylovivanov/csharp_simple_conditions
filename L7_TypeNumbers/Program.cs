01378441495d3e6a02c44072e5d3e1ee1a968212
ivaylovivanov <ivalentinov@gmail.com>
2016-06-03 00:59:14 +0300

    L7

---
+++ L7_TypeNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L7_TypeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+            string[] words = new string[10] {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+            
+            if (Enumerable.Range(0,10).Contains(number))
+            {
+                Console.WriteLine(words[number]);     
+            }
+            else
+            {
+                Console.WriteLine("number too big");
+            }
+
+        }
+    }
+}
