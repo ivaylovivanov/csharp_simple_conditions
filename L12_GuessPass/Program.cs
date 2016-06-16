3e92eb03f6c683d6ed3e5e9b3fb47ac755e69f41
ivaylovivanov <ivalentinov@gmail.com>
2016-06-16 23:34:01 +0300

    L12

---
+++ L12_GuessPass/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L12_GuessPass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            if (input.ToLower() == "s3cr3t!P@ssw0rd".ToLower())
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+
+        }
+    }
+}
