43e835b215aa55b04ecf860ff084a867e6b5451e
ivaylovivanov <ivalentinov@gmail.com>
2016-06-17 00:13:11 +0300

    L13 - L15

---
+++ L14_SameWords/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L14_SameWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input1 = Console.ReadLine();
+            var input2 = Console.ReadLine();
+            if (input1.ToLower() == input2.ToLower())
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
