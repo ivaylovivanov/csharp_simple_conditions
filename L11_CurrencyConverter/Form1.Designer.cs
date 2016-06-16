581750635db0b80110f310ef1a9c8874db7aa9f4
ivaylovivanov <ivalentinov@gmail.com>
2016-06-16 23:22:48 +0300

    L11

--- L11_CurrencyConverter/Form1.Designer.cs
+++ L11_CurrencyConverter/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace L11_CurrencyConverter
 {
-    partial class Form1
+    partial class FormConverter
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.numericUpDownAmount = new System.Windows.Forms.NumericUpDown();
+            this.comboBoxCurrency = new System.Windows.Forms.ComboBox();
+            this.labelResult = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAmount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDownAmount
+            // 
+            this.numericUpDownAmount.DecimalPlaces = 2;
+            this.numericUpDownAmount.Location = new System.Drawing.Point(39, 60);
+            this.numericUpDownAmount.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.numericUpDownAmount.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDownAmount.Name = "numericUpDownAmount";
+            this.numericUpDownAmount.Size = new System.Drawing.Size(178, 26);
+            this.numericUpDownAmount.TabIndex = 0;
+            this.numericUpDownAmount.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numericUpDownAmount.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownAmount.ValueChanged += new System.EventHandler(this.numericUpDownAmount_ValueChanged);
+            // 
+            // comboBoxCurrency
+            // 
+            this.comboBoxCurrency.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxCurrency.FormattingEnabled = true;
+            this.comboBoxCurrency.Items.AddRange(new object[] {
+            "EUR",
+            "USD",
+            "GBP"});
+            this.comboBoxCurrency.Location = new System.Drawing.Point(273, 60);
+            this.comboBoxCurrency.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboBoxCurrency.Name = "comboBoxCurrency";
+            this.comboBoxCurrency.Size = new System.Drawing.Size(180, 28);
+            this.comboBoxCurrency.TabIndex = 1;
+            this.comboBoxCurrency.SelectedIndexChanged += new System.EventHandler(this.comboBoxCurrency_SelectedIndexChanged);
+            // 
+            // labelResult
+            // 
+            this.labelResult.BackColor = System.Drawing.Color.LimeGreen;
+            this.labelResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelResult.Location = new System.Drawing.Point(34, 135);
+            this.labelResult.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelResult.MaximumSize = new System.Drawing.Size(525, 77);
+            this.labelResult.MinimumSize = new System.Drawing.Size(300, 20);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(420, 31);
+            this.labelResult.TabIndex = 2;
+            this.labelResult.Text = "label1";
+            this.labelResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FormConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(472, 206);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.comboBoxCurrency);
+            this.Controls.Add(this.numericUpDownAmount);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormConverter";
+            this.Text = "Currency Converter";
+            this.Load += new System.EventHandler(this.FormConverter_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAmount)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDownAmount;
+        private System.Windows.Forms.ComboBox comboBoxCurrency;
+        private System.Windows.Forms.Label labelResult;
     }
 }
 
--- L11_CurrencyConverter/Form1.cs
+++ L11_CurrencyConverter/Form1.cs
@@ -10,11 +10,47 @@ using System.Windows.Forms;
 
 namespace L11_CurrencyConverter
 {
-    public partial class Form1 : Form
+    public partial class FormConverter : Form
     {
-        public Form1()
+        public FormConverter()
         {
             InitializeComponent();
+        }
+
+        private void numericUpDownAmount_ValueChanged(object sender, EventArgs e)
+        {
+            ConvertCurrency();
+        }
+
+        private void comboBoxCurrency_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ConvertCurrency();
+        }
+
+        private void FormConverter_Load(object sender, EventArgs e)
+        {
+            this.comboBoxCurrency.SelectedItem = "EUR";
+        }
+
+        private void ConvertCurrency()
+        {
+            var originalAmount = this.numericUpDownAmount.Value;
+            var convertedAmount = originalAmount;
+            if (this.comboBoxCurrency.SelectedItem.ToString() == "EUR")
+            {
+                convertedAmount = originalAmount / 1.95583m;
+            }
+            if (this.comboBoxCurrency.SelectedItem.ToString() == "USD")
+            {
+                convertedAmount = originalAmount / 1.80810m;
+            }
+            if (this.comboBoxCurrency.SelectedItem.ToString() == "GBP")
+            {
+                convertedAmount = originalAmount / 2.54990m;
+            }
+
+            this.labelResult.Text = originalAmount + " лв. " +
+                Math.Round(convertedAmount, 2) + " " + this.comboBoxCurrency.SelectedItem;
         }
     }
 }
